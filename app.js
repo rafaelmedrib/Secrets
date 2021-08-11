9ff109fae49d825a6d7de1b15e1af8148fd6f42e
rafaelmedrib <rafaelmedrib@gmail.com>
2021-08-10 22:38:45 -0300

    Add logout route

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ app.get("/secrets", function(req, res){
   }
 });
 
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
 app.post("/register", function(req, res){
   User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err){
